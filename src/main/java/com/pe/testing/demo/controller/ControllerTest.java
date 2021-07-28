031571afbea054a0c45c3d662919370e40cc4aa6
Jhon Larru <larru93@gmailc.om>
2021-07-28 00:38:55 -0500

    123

--- src/main/java/com/pe/testing/demo/controller/ControllerTest.java
+++ src/main/java/com/pe/testing/demo/controller/ControllerTest.java
@@ -3,10 +3,9 @@ package com.pe.testing.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller()
-@CrossOrigin(origins = "*")
+@RestController("/")
 public class ControllerTest {
     @GetMapping()
     public String test(){
